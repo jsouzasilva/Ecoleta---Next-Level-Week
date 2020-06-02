688bb4f97e603b0254d14ce09a637b5afdd5eb19
= <=>
2020-06-02 16:56:57 -0300

    database creation sqlite3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,10 @@
-import express from "express";
+import express, { json } from "express";
+import routes from "./routes";
 
 const app = express();
 
-//app.use(express.json());
-app.get("/", (request, response) => {
-    response.send(["juliano", "Edezio"]);
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333);
