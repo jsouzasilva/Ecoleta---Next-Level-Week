6cc3072a345355f158ba5b80e2ee51d6e8d7a4ce
= <=>
2020-06-03 22:14:49 -0300

    creation of seeds for the initial population of the database. Creation of registration routes and lists of collection points.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
 const routes = Router();
-routes.get("/", (request, response) => {
-    return response.json({ message: "Hello Word" });
-});
 
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+routes.get("/items", itemsController.index);
+
+routes.post("/points", pointsController.create);
+routes.get("/points", pointsController.index);
+routes.get("/points/:id", pointsController.show);
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import express, { json } from "express";
 import routes from "./routes";
 
@@ -7,4 +8,5 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 app.listen(3333);
