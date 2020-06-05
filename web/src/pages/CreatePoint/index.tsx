e68f9227d71830052816f73f7ae39d08fa427a69
= <=>
2020-06-05 01:57:04 -0300

    integration of the map into the component of registration of collection points. - React

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, {
+    useState,
+    useEffect,
+    ChangeEvent,
+    SelectHTMLAttributes,
+} from 'react'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
-import logo from '../../assets/logo.svg'
 
+import { Map, TileLayer, Marker } from 'react-leaflet'
+
+import axios from 'axios'
+import api from '../../services/api'
+
+import logo from '../../assets/logo.svg'
 import './style.css'
+
 const CreatePoint: React.FC = () => {
+    interface Item {
+        id: number
+        image_url: string
+        name: string
+    }
+    interface IBGEUFResponse {
+        sigla: string
+    }
+    interface IBGECityResponse {
+        nome: string
+    }
+
+    const [items, setItems] = useState<Item[]>([])
+    const [ufs, setUfs] = useState<string[]>([])
+    const [cities, setCities] = useState<string[]>([])
+    const [selectedUf, setSelectedUf] = useState('0')
+    useEffect(() => {
+        api.get('items').then((response) => {
+            setItems(response.data)
+        })
+    }, [])
+
+    useEffect(() => {
+        axios
+            .get<IBGEUFResponse[]>(
+                'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+            )
+            .then((response) => {
+                const ufInitials = response.data.map((uf) => uf.sigla)
+                setUfs(ufInitials)
+            })
+    }, [])
+    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+        const uf = event.target.value
+        setSelectedUf(uf)
+    }
+
+    useEffect(() => {
+        if (selectedUf === '0') {
+            return
+        }
+
+        axios
+            .get<IBGECityResponse[]>(
+                `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+            )
+            .then((response) => {
+                const cityNames = response.data.map((city) => city.nome)
+                setCities(cityNames)
+            })
+    }, [selectedUf])
     return (
         <div id="page-create-point">
             <header>
@@ -30,27 +92,74 @@ const CreatePoint: React.FC = () => {
                         <label htmlFor="name">Nome da entidade</label>
                         <input type="text" name="name" id="name" />
                     </div>
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">E-mail</label>
+                            <input type="email" name="email" id="email" />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="whatsapp">Whatsapp</label>
+                            <input type="text" name="whatsapp" id="whatsapp" />
+                        </div>
+                    </div>
                 </fieldset>
                 <fieldset>
                     <legend>
                         <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa</span>
                     </legend>
+                    <Map center={[-15.7531375, -48.2891368]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-15.7531375, -48.2891368]} />
+                    </Map>
                     <div className="field-group">
                         <div className="field">
-                            <label htmlFor="email">E-mail</label>
-                            <input type="text" name="name" id="name" />
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select
+                                name="uf"
+                                id="uf"
+                                value={selectedUf}
+                                onChange={handleSelectUf}
+                            >
+                                <option value="0">Selecione uma UF</option>
+                                {ufs.map((uf) => (
+                                    <option key={uf} value={uf}>
+                                        {uf}
+                                    </option>
+                                ))}
+                            </select>
                         </div>
                         <div className="field">
-                            <label htmlFor="name">Nome da entidade</label>
-                            <input type="text" name="name" id="name" />
+                            <label htmlFor="city">Estado (UF)</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidade</option>
+                                {cities.map((city) => (
+                                    <option key={city} value={city}>
+                                        {city}
+                                    </option>
+                                ))}
+                            </select>
                         </div>
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend>
                         <h2>Ítens de coleta</h2>
+                        <span>Selecione um ou mais ítens abaixo</span>
                     </legend>
+                    <ul className="items-grid">
+                        {items.map((item) => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.name} />
+                                <span>{item.name}</span>
+                            </li>
+                        ))}
+                    </ul>
                 </fieldset>
+                <button type="submit">Cadastrar</button>
             </form>
         </div>
     )
