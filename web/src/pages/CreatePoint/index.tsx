da8e420a5f0ff0d683d82310ee6a61237fa785a7
= <=>
2020-06-04 23:52:19 -0300

    Configuring routes and creating home page components - React

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import { FiArrowLeft } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.svg'
 
+import './style.css'
 const CreatePoint: React.FC = () => {
     return (
-        <div>
-            <h1>Hello planet</h1>
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta" />
+                <Link to="/">
+                    <FiArrowLeft />
+                    voltar para Home
+                </Link>
+            </header>
+
+            <form>
+                <h1>
+                    Cadastro do <br />
+                    ponto de coleta
+                </h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+
+                    <div className="field">
+                        <label htmlFor="name">Nome da entidade</label>
+                        <input type="text" name="name" id="name" />
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                    </legend>
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">E-mail</label>
+                            <input type="text" name="name" id="name" />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="name">Nome da entidade</label>
+                            <input type="text" name="name" id="name" />
+                        </div>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <legend>
+                        <h2>Ítens de coleta</h2>
+                    </legend>
+                </fieldset>
+            </form>
         </div>
     )
 }
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+
 import './style.css'
 import logo from '../../assets/logo.svg'
 const Home: React.FC = () => {
@@ -15,10 +17,12 @@ const Home: React.FC = () => {
                         Ajudamos pessoas a encontrarem pontos de coleta de forma
                         eficiente.
                     </p>
-                    <a href="/cadastro">
-                        <span>:</span>{' '}
+                    <Link to="/create-point">
+                        <span>
+                            <FiLogIn />
+                        </span>
                         <strong>Cadastre um ponto de coleta</strong>
-                    </a>
+                    </Link>
                 </main>
             </div>
         </div>
