332ec25a5cd0a3f1dd7961b5fdc87e93281f742b
= <=>
2020-06-04 23:35:38 -0300

    Configuring routes and creating home page components

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePoint: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello planet</h1>
+        </div>
+    )
+}
+
+export default CreatePoint
